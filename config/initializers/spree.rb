b79126820f2b3a5b40ec35a02305a37d07385a4c
Apple <apple@Apples-MacBook-Pro.local>
2024-05-31 09:47:07 -0500

    update configuration

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,12 +1,12 @@
 Rails.application.config.after_initialize do
 
   items = [
-  	Spree::Admin::MainMenu::ItemBuilder.new('Blogs', Spree::Core::Engine.routes.url_helpers.admin_blogs_path()).build,
-    Spree::Admin::MainMenu::ItemBuilder.new('Posts', Spree::Core::Engine.routes.url_helpers.admin_posts_path()).build
+  	Spree::Admin::MainMenu::ItemBuilder.new(I18n.t('spree_blogs.admin.blogs'), Spree::Core::Engine.routes.url_helpers.admin_blogs_path()).build,
+    Spree::Admin::MainMenu::ItemBuilder.new(I18n.t('spree_blogs.admin.posts'), Spree::Core::Engine.routes.url_helpers.admin_posts_path()).build
   ]
 
   Rails.application.config.spree_backend.main_menu.add(
-    Spree::Admin::MainMenu::SectionBuilder.new(Spree.t(:blog), 'blogs.svg').
+    Spree::Admin::MainMenu::SectionBuilder.new(I18n.t('spree_blogs.admin.blogs'), 'blogs.svg').
       with_admin_ability_check(Spree::Blog).
       with_items(items).
       build
