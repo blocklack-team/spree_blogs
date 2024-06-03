90c5777596f0184d77ea9570e2047c360d583fb6
Apple <apple@Apples-MacBook-Pro.local>
2024-06-03 17:53:00 -0500

    update posts

--- app/controllers/spree/api/post_controller.rb
+++ app/controllers/spree/api/post_controller.rb
@@ -1,19 +1,23 @@
-class Spree::Api::PostsController < ::Spree::Api::V2::ResourceController
-	before_action :require_spree_current_user, except: [:show]
+module Api
+	module Spree
+		class PostsController < ::Spree::Api::V2::ResourceController
+			before_action :require_spree_current_user, except: [:show]
 
-  def show
-    @blog = Spree::Blog.by_store(current_store).friendly.find(params[:blog_id])
-
-    @post = if try_spree_current_user.try(:has_spree_role?, "admin")
-      @blog.posts.find(params[:id])
-    else
-      @blog.posts.published_and_visible.find(params[:id])
-    end
-
-    @title = @post.post_seo_title
-
-    respond_to do |format|
-      format.json { render json: @post, status: :ok }
-    end
-  end
+			def show
+				@blog = Spree::Blog.by_store(current_store).friendly.find(params[:blog_id])
+		
+				@post = if try_spree_current_user.try(:has_spree_role?, "admin")
+					@blog.posts.find(params[:id])
+				else
+					@blog.posts.published_and_visible.find(params[:id])
+				end
+		
+				@title = @post.post_seo_title
+		
+				respond_to do |format|
+					format.json { render json: @post, status: :ok }
+				end
+			end
+		end
+	end
 end
