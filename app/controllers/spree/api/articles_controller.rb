1287c53ce502a9a8c379a2e807f3c43a793bc107
Apple <apple@Apples-MacBook-Pro.local>
2024-06-03 22:06:39 -0500

    update posts

--- app/controllers/spree/api/articles_controller.rb
+++ app/controllers/spree/api/articles_controller.rb
@@ -1,17 +1,28 @@
-class Spree::Api::ArticlesController < Spree::StoreController
-	def show
-		@blog = Spree::Blog.friendly.find(params[:blog_id])
+class Spree::Api::ArticlesController < ::Spree::Api::V2::ResourceController
+	before_action :require_spree_current_user, except: [:index, :show]
+	before_action :load_product, only: [:index, :show]
 
-		@post = if try_spree_current_user.try(:has_spree_role?, "admin")
-			@blog.posts.find(params[:id])
+	def index
+		posts = @blog.posts.published_and_visible
+	  
+		if posts.present?
+			render json: posts, status: :ok
 		else
-			@blog.posts.published_and_visible.find(params[:id])
+			render json: { errors: 'No articles found' }, status: :ok
 		end
+	end
 
-		@title = @post.post_seo_title
+	def show
+		@post = @blog.posts.published_and_visible.find(params[:id])
 
 		respond_to do |format|
 			format.json { render json: @post, status: :ok }
 		end
+	end
+
+	private
+
+	def load_product
+		@blog = Spree::Blog.friendly.find(params[:blog_id])
 	end
 end
